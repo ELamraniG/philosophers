dc9ab211b2d31dd1e05a2fafc2cf5e1e45839dd8
ELamraniG <moha.mati@hotmail.com>
2025-07-03 17:09:33 +0100

    start philo bonus

---
+++  philo_bonus/threads.h
@@ -0,0 +1,10 @@
+#ifndef THREADS_H
+# define THREADS_H
+#endif
+
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
---
+++ philo/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-amr <moel-amr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 20:17:01 by moel-amr          #+#    #+#             */
+/*   Updated: 2025/07/01 20:17:40 by moel-amr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "threads.h"
+
+static int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int	free_boring(void)
+{
+	printf("invalide args\n");
+	return (-1);
+}
+
+int	ft_atoi(const char *s)
+{
+	long res;
+	int i;
+
+	i = 0;
+	res = 0;
+	if (s[i] == '+')
+		i++;
+	if (!ft_isdigit(s[i]))
+		return (free_boring());
+	while (ft_isdigit(s[i]))
+	{
+		res = ((res * 10) + (s[i] - 48));
+		i++;
+		if (s[i] != 0 && !ft_isdigit(s[i]))
+			return (free_boring());
+	}
+	if (res > 2147483647 || res < -2147483648)
+		return (free_boring());
+	return (res);
+}
